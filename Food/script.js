069c92a41afe49b3b6efd3ce7607a3af97962fff
Luk4om <halls12345ham@gmail.com>
2024-02-27 16:32:53 +0700

    Reset

---
+++ Food/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", loadData())
+
+function saveData() {
+    const key = document.getElementById("inputKey").value;
+    const value = document.getElementById("inputValue").value;
+    localStorage.setItem(key, value);
+    document.getElementById("inputKey").value = "";
+    document.getElementById("inputKey").value = "";
+    loadData();
+    alert("Data saved 🫡")
+}
+
+function clearData() {
+    localStorage.clear();
+    loadData();
+    alert("All data cleared successfully")
+}
+
+function loadData() {
+    const container = document.getElementById("data");
+    container.innerHTML = "";
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i)
+        const value = localStorage.getItem(key)
+
+        const div = document.createElement("div")
+        div.textContent = `Name: ${key}, Value: ${value}`;
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.onclick = function () {
+            localStorage.removeItem(key);
+            loadData()
+        };
+
+        div.appendChild(removeButton);
+        container.appendChild(div);
+    }
+}
--- js/work.js
+++
@@ -1,5 +0,0 @@
-let stampPrice = 30 ;
-let pillowPrice = 1 ;
-let emmaStamp = 33 ;
-
-console.log(`Emma need ${(((50 -emmaStamp)*stampPrice)+pillowPrice)} money`)
