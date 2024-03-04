ebd313acb6ab03c6cc453669c06cb32be6abd484
Luk4om <halls12345ham@gmail.com>
2024-03-04 16:42:25 +0700

    fix

--- Store/app.js
+++ Store/app.js
@@ -1,22 +1,13 @@
 const express = require("express");
-const bodyparser = require("body-parser");
-
-// middleware
-const app = express();
+const bodyParser = require("body-parser");
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyparser.urlencoded({ extended: false }));
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin",adminRoutes)
-app.use(shopRoutes);
+app.use('/admin',adminRoutes); // Using adminRoutes for paths starting with '/admin'
+app.use(shopRoutes); // Using shopRoutes for paths starting with '/'
 
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found, See the VAR!</h1>');
-});
-
-// const server = http.createServer(app);
-// server.listen(3000);
-
-app.listen(3000);
+app.listen(3000)
--- Store/routes/shop.js
+++ Store/routes/shop.js
@@ -1,9 +1,8 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
 
-const routes = express.Routes()
-
-routes.get((req, res, next) => {
-    res.send("<h1>Welcome to My Shop</h1>");
+router.get("/", (req, res, next) => {
+  res.send('<h1>Welcome to My Shop</h1>');
 });
 
 module.exports = router;
